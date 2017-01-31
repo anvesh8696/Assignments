e8217037af0c56f1ea580b469d4d02bd6a70f30e
Jayanth Kothari <jayanthkothari@Jayanths-MacBook-Air.local>
2017-01-30 20:23:12 -0600

    Jayanth assignment 3

---
+++ Jayanth/Assignment3/Assignment3.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const App = React.createClass({
+
+const events = [];
+
+      componentWillMount() {
+          const events = this.state.events;
+          events.push("Mounting component");
+      },
+
+      componentDidMount() {
+          const events = this.state.events;
+          events.push("Component mounted");
+          this.setState({events});
+      },
+
+      render() {
+          const eventNodes = this.state.events.map(event => <li>{event}</li>);
+          return (
+              <ul>
+              {eventNodes}
+              </ul>
+          );
+      }
+  });
+  
+export default App;
